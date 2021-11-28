a1aaf1038bd09b38fccfe5122021d32880b0abdc
Tom Clark <tom@runawayplay.com>
2021-11-29 12:16:38 +1300

    added a main

--- 14-sequences/14-practical/q2.py
+++ 14-sequences/14-practical/q2.py
@@ -5,3 +5,22 @@
 # methods. Your new Queue should implement collections.abc.Sequence.
 # Include an "if __name__ == '__main__':" section demonstrating
 # the sequence properties.
+
+
+class Queue:
+    pass
+
+
+if __name__ == '__main__':
+
+    q = Queue()
+    for i in range(10):
+        q.enqueue(i)
+
+    print(q[5])
+    q_slice = q[3:6]
+    if isinstance(q_slice, Queue):
+        print('A slice of a Queue is also a Queue')
+    else:
+        print('Something is not right.')
+    print(q_slice[1])    
